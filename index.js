2e7d74f2297216b543d3349a9bf0e20d22ccad13
Hasib24 <hasibislam1488@gmail.com>
2023-05-03 21:50:47 +0600

    configure to get chef data besd on id

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const express = require('express');
 const app = express();
 const port = 5000;
+const cors = require('cors')
 
-const chef = require('./data/shef.json')
+app.use(cors())
+
+const chefs = require('./data/shefs.json')
 
 app.get('/', (req, res)=>{
     res.send('Express server is running!')
 })
 
 app.get('/chef', (req, res)=>{
-    res.send(chef)
+    res.send(chefs)
 })
+
+app.get('/chefrecipes/:id', (req, res)=>{
+    let id = req.params.id;
+    res.send(chefs.find(chef => chef.id == id))
+})
+
 
 
 app.listen(port, ()=>{
