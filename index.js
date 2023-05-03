62b87999b5d97693908c42fb3c6e797138dbf97c
Hasib24 <hasibislam1488@gmail.com>
2023-05-03 18:29:25 +0600

    update json data and make loadabe with api

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+const chef = require('./data/shef.json')
+
 app.get('/', (req, res)=>{
     res.send('Express server is running!')
 })
+
+app.get('/chef', (req, res)=>{
+    res.send(chef)
+})
+
 
 app.listen(port, ()=>{
     console.log(`Server is running at port : ${port}`);
